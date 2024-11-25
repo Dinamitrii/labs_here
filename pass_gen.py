2484b5511480ebd902c359196b7543ddec82ee02
dinamitrii <k_karaivanov@abv.bg>
2024-11-25 17:49:13 +0200

    fine tune - added a timer for the actual password generation time.

--- pass_gen.py
+++ pass_gen.py
@@ -3,11 +3,16 @@ import string
 from sys import maxsize
 import pyperclip
 import textwrap
+import time
+
+
 
 minsize = abs(6)
 maxsize = abs(maxsize - minsize)
 given_str = ""
 action = ""
+
+
 
 length = input(f"How long do you want password to be?..."
                f"[minsize of {minsize} symbols]..."
@@ -23,21 +28,26 @@ while length is None or length == "" or int(length) < minsize:
                    f"[maxsize of -->{maxsize}<-- consecutive symbols but more time consuming generation.]\n")
 
 while minsize <= int(length) <= maxsize or action.lower() == "y":
+
+    starting_time_zero_point = time.time()
+
     given_str = ""
 
     for char in range(int(length)):
+        
         upper = random.choice(ascii(string.ascii_uppercase))
         lowerr = random.choice(ascii(string.ascii_lowercase))
         nums = random.choice(ascii(string.digits))
         symbols = random.choice(ascii(string.punctuation))
 
         given_str += ("".join(map(str, random.sample(upper + lowerr + nums + symbols, 1)[-1])))
-
+    endtime_zero_point = time.time()
+        
     print(f"Your desired length of password is -->{len(given_str)}<-- symbols "
           f"of maximum possible -->{maxsize}<-- consecutive symbols but more time consuming generation.\n")
 
-    print(f"Your Password is from here -->{textwrap.fill(given_str, width=162)}<-- to here exactly between the arrows.\n")
-
+    print(f"Your Password is from here -->{textwrap.fill(given_str, width=162)}<-- to here exactly "
+          f"between the arrows.\n")
 
     action = input(f"Do you want to use again with new sample 'Y'es / 'N'o / 'E'xport to a "
                    f"'password_gen.txt' file for convenience...Enter your choice...:\n").lower().strip()
@@ -45,24 +55,27 @@ while minsize <= int(length) <= maxsize or action.lower() == "y":
     if action.lower() == "e":
         pyperclip.copy(given_str)
         open('password_gen.txt', 'w').writelines(pyperclip.paste())
-
         print("Exported to 'password_gen.txt'")
+
+        print(f"Until exporting it took exactly {(endtime_zero_point - starting_time_zero_point):.3f} seconds to generate the password.")
         break
 
     elif action.lower() == "n":
         pyperclip.copy(given_str)
         open('password_gen.txt', 'w').writelines(pyperclip.paste())
-
         print("Exported to 'password_gen.txt' by system settings")
+
+        print(f"Until exiting it took exactly {(endtime_zero_point - starting_time_zero_point):.3f}seconds to generate the password.")
         break
 
     elif action.lower() == "y":
-
         length = input(f"How long do you want password to be?..."
-                       f"[minsize of {minsize} symbols.]..."
+                       f"[minsize of {minsize} symbols]..."
                        f"[maxsize of -->{maxsize}<-- consecutive symbols but more time consuming generation.]!!!\n")
-
         length = int(length)
         continue
     else:
         action = action.lower() == "n"
+
+endtime_zero_point = time.time()
+print(f"Your FUN continued for {(endtime_zero_point - starting_time_zero_point):.3f} seconds.")
--- python_map.py
+++ python_map.py
@@ -1,3 +1,13 @@
+# import folium package and create map
 import folium
-from IPython.display import display
 
+
+def generate_geo_map():
+    generated_map = folium.Map(location=[42.6975, 23.3242], zoom_start=12)
+
+    # Pass a string in popup parameter
+    folium.Marker([42.6975, 23.3242], popup='https://predictorian.online').add_to(generated_map)
+
+    generated_map.save("generated_map.html")
+
+generate_geo_map()
