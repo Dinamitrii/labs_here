32467935ca3db0d8a4ef3cc3c79ec3fd1141fe7e
dinamitrii <k_karaivanov@abv.bg>
2025-08-16 02:29:23 +0300

    trying YT downloader

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,11 +1,14 @@
 from pytube import YouTube
 
+link = input("Enter YT link:'    '")
+
 def download(link):
     try:
         video = YouTube(link)
-        video = video.streams.get_highest_resolution()
-        video.download()
+        video = video.streams.filter(file_extension="mp4").get_highest_resolution()
+        video.download(link)
         print("Video downloaded")
 
     except:
-        print("Video not found")
+        print("Video not downloaded")
+
