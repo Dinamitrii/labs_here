77c82d2b206f2b536058aa3aa999099a0276d6c7
dinamitrii <k_karaivanov@abv.bg>
2025-08-29 02:04:51 +0300

    gpd map plot

--- geomap_gpd/geomap_gpd.py
+++ geomap_gpd/geomap_gpd.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-world_map = gpd.read_file("ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp")
+world_map = gpd.read_file("geomap_gpd/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp")
 
 world_map.plot(edgecolor="orange")
 plt.title("World Map with Country Borders")
