9d6b6013862af7a23b31642b4fcfc0ef79fe72d1
dinamitrii <k_karaivanov@abv.bg>
2025-10-20 03:16:38 +0300

    just no wtd

--- iptracer/asyncio_trace.py
+++ iptracer/asyncio_trace.py
@@ -4,7 +4,7 @@ import mtrpacket
 async def trace():
     async with mtrpacket.MtrPacket() as mtr:
         for ttl in range(1, 256):
-            result = await mtr.probe('example.com', ttl=ttl)
+            result = await mtr.probe('abv.bg', ttl=ttl)
             print(result)
 
             if result.success:
